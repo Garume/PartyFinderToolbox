e91d0b409ec975489bdcdbf7f828b599cbac9034
ガルム <desuboruto@gmail.com>
2025-01-05 15:06:51 +0900

    [add] debug

--- PartyFiltering/Core/UI/DebugTab.cs
+++ PartyFiltering/Core/UI/DebugTab.cs
@@ -6,31 +6,42 @@ namespace PartyFinderToolbox.Core.UI;
 
 public class DebugTab : Tab
 {
+    private int _dutyCategory;
+    private int _dutyId;
     public override string Name => "Debug";
 
     public override unsafe void Draw()
     {
         ImGui.Text("Debug");
-        var storedInfo = AgentLookingForGroup.Instance()->StoredRecruitmentInfo;
-        ImGui.Text($"DutyCategory:{storedInfo.SelectedCategory}");
-        ImGui.Text($"DutyID:{storedInfo.SelectedDutyId}");
-        ImGui.Text($"Objective:{storedInfo.Objective}");
-        ImGui.Text($"BeginnerFriendly:{storedInfo.BeginnerFriendly}");
-        ImGui.Text($"CompletionStatus:{storedInfo.CompletionStatus}");
-        ImGui.Text($"DutyFinderSettingFlags:{storedInfo.DutyFinderSettingFlags}");
-        ImGui.Text($"LootRule:{storedInfo.LootRule}");
-        ImGui.Text($"Password:{storedInfo.Password}");
-        ImGui.Text($"NumberOfSlotsInMainParty:{storedInfo.NumberOfSlotsInMainParty}");
-        ImGui.Text($"LimitRecruitingToWorld:{storedInfo.LimitRecruitingToWorld}");
-        ImGui.Text($"OnePlayerPerJob:{storedInfo.OnePlayerPerJob}");
-        ImGui.Text($"NumberOfGroups:{storedInfo.NumberOfGroups}");
+        var storedInfo = &AgentLookingForGroup.Instance()->StoredRecruitmentInfo;
+        ImGui.Text($"DutyCategory:{storedInfo->SelectedCategory}");
+        ImGui.Text($"DutyID:{storedInfo->SelectedDutyId}");
+        ImGui.Text($"Objective:{storedInfo->Objective}");
+        ImGui.Text($"BeginnerFriendly:{storedInfo->BeginnerFriendly}");
+        ImGui.Text($"CompletionStatus:{storedInfo->CompletionStatus}");
+        ImGui.Text($"DutyFinderSettingFlags:{storedInfo->DutyFinderSettingFlags}");
+        ImGui.Text($"LootRule:{storedInfo->LootRule}");
+        ImGui.Text($"Password:{storedInfo->Password}");
+        ImGui.Text($"NumberOfSlotsInMainParty:{storedInfo->NumberOfSlotsInMainParty}");
+        ImGui.Text($"LimitRecruitingToWorld:{storedInfo->LimitRecruitingToWorld}");
+        ImGui.Text($"OnePlayerPerJob:{storedInfo->OnePlayerPerJob}");
+        ImGui.Text($"NumberOfGroups:{storedInfo->NumberOfGroups}");
 
-        for (var i = 0; i < storedInfo.MemberContentIds.Length; i++)
-            ImGui.Text($"MemberContentIds[{i}]:{storedInfo.MemberContentIds[i]}");
+        for (var i = 0; i < storedInfo->MemberContentIds.Length; i++)
+            ImGui.Text($"MemberContentIds[{i}]:{storedInfo->MemberContentIds[i]}");
 
-        for (var i = 0; i < storedInfo.SlotFlags.Length; i++)
-            ImGui.Text($"MemberJobIds[{i}]:{storedInfo.SlotFlags[i]}");
+        for (var i = 0; i < storedInfo->SlotFlags.Length; i++)
+            ImGui.Text($"MemberJobIds[{i}]:{storedInfo->SlotFlags[i]}");
 
-        ImGui.Text($"Comment:{storedInfo.CommentString}");
+        ImGui.Text($"Comment:{storedInfo->CommentString}");
+
+        ImGui.Text("Set");
+        ImGui.Indent();
+        ImGui.InputInt("DutyCategory", ref _dutyCategory);
+        if (ImGui.Button("Set DutyCategory"))
+            storedInfo->SelectedCategory = (ushort)_dutyCategory;
+        ImGui.InputInt("DutyID", ref _dutyId);
+        if (ImGui.Button("Set DutyID"))
+            storedInfo->SelectedDutyId = (ushort)_dutyId;
     }
 }
