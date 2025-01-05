c1195082801fbdf5923dd65867f4a1604caf05cd
ガルム <desuboruto@gmail.com>
2025-01-05 15:06:17 +0900

    [add] select yes no addon

---
+++ PartyFiltering/Shared/UI/Addon/SelectYesNo.cs
@@ -0,0 +1,22 @@
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using PartyFinderToolbox.Shared.Utility;
+
+namespace PartyFinderToolbox.Shared.UI.Addon;
+
+public  unsafe class SelectYesNo(nint addon)
+{
+    private AtkUnitBase* Base { get; } = (AtkUnitBase*)addon;
+
+    public AtkComponentButton* YesButton => Base->GetButtonNodeById(8);
+    public AtkComponentButton* NoButton => Base->GetButtonNodeById(11);
+
+    public void Yes()
+    {
+        YesButton->Click(Base);
+    }
+
+    public void No()
+    {
+        NoButton->Click(Base);
+    }
+}
