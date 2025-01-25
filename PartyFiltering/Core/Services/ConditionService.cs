801e64e58617efde8c737d83cd684528760cca55
ガルム <desuboruto@gmail.com>
2025-01-25 21:07:02 +0900

    [add] condition service

---
+++ PartyFiltering/Core/Services/ConditionService.cs
@@ -0,0 +1,19 @@
+﻿using Dalamud.Game.ClientState.Conditions;
+using Dalamud.IoC;
+using Dalamud.Plugin.Services;
+using Lumina.Excel;
+using PartyFinderToolbox.Shared.Services;
+
+namespace PartyFinderToolbox.Core.Services;
+
+[LoadService]
+public class ConditionService : Service<ConditionService>
+{
+    [PluginService] private static ICondition Condition { get; set; } = null!;
+
+
+    public static bool IsUsingPartyFinder()
+    {
+        return Condition[ConditionFlag.UsingPartyFinder];
+    }
+}
